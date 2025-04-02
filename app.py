ceaae5325fcd71f575065d3b8c9df29caa04b5d3
AdriatikIMB <156903049+AdriatikIMB@users.noreply.github.com>
2025-04-02 22:05:34 +0200

    legg til bruker

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
-# Funksjon for å koble til databasen
 def get_db_connection():
     return mysql.connector.connect(
         user="adriatik",
         password="Adriatik.123",
         database="restaurant",
         host="10.2.4.76",
-        port=3306
     )
-
+    
 @app.route('/')
 def index():
-    return render_template('index.html')  # Skjemaet som skal vises i nettleseren
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    
+    return render_template('index.html', users=users)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        username = request.form['username']  # Bruker 'username' i stedet for 'name'
-        password = request.form['password']  # Bruker 'password' i stedet for 'email'
-        
+        username = request.form['username']
+        password = request.form['password']
         conn = get_db_connection()
         cursor = conn.cursor()
-        
-        # Sett inn data i users-tabellen
         cursor.execute("INSERT INTO user (username, password) VALUES (%s, %s)", (username, password))
         conn.commit()
-        
         cursor.close()
         conn.close()
-        
-        return redirect(url_for('index'))  # Etter innsending, vis skjemaet igjen
+        return redirect(url_for('index'))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
+
